553d268eeb51f2a022ec858ba26cd2be963a4c26
Ayoubbhn <Ayoubbouhanni1998@gmail.com>
2023-03-27 08:11:30 -0700

    task 6

--- 0x05-pointers_arrays_strings/3-main.c
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -1,14 +1,11 @@
 #include "main.h"
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
 int main(void)
 {
-char *str;
-str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-_puts(str);
-return (0);
+	char *str;
+
+	str = "I do not fear computers. i fear the lack of them - isaac Asimov";
+	_puts(str);
+	return (0);
 }
+
